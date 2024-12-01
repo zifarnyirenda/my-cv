8a396c8633762115eab81f48be723b581ee99328
zifarnyirenda <zifarchishalanyirenda@gmail.com>
2024-12-01 02:44:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function contactMe() {
+    window.location.href = "mailto:zifarchishalanyirenda@gmail.com";
+}
+
+document.getElementById('linkedin-btn').href = "https://www.linkedin.com/in/zifar-nyirenda";
