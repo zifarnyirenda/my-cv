446e57f835815b5f90d4cc130dd6c7161677e1b7
zifarnyirenda <zifarchishalanyirenda@gmail.com>
2024-12-12 22:17:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Function to open email client
 function contactMe() {
     window.location.href = "mailto:zifarchishalanyirenda@gmail.com";
 }
 
-// Dynamically set LinkedIn URL (if needed)
 document.getElementById('linkedin-btn').href = "https://www.linkedin.com/in/zifar-nyirenda";
 
-// Animation on page load
 document.addEventListener('DOMContentLoaded', () => {
     const profile = document.querySelector('.header');
     profile.style.transform = 'translateY(0)';
