cd6198593ddfac8a3b83ef62478cad36f83c6145
zifarnyirenda <zifarchishalanyirenda@gmail.com>
2025-02-03 19:18:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ function contactMe() {
     window.location.href = "mailto:zifarchishalanyirenda@gmail.com";
 }
 
-document.getElementById('linkedin-btn').href = "http://linkedin.com/in/zifar-nyirenda-086b1a237";
+
+document.getElementById('linkedin-btn').href = "https://www.linkedin.com/in/zifar-nyirenda";
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const profile = document.querySelector('.header');
-    profile.style.transform = 'translateY(0)';
+      
+    profile.style.opacity = 0;
+    profile.style.transform = 'translateY(-20px)';
+    
+    setTimeout(() => {
+        profile.style.transition = 'all 0.6s ease-out';
+        profile.style.opacity = 1;
+        profile.style.transform = 'translateY(0)';
+    }, 100);
 });
