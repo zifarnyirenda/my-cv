a82779550e836b0a36bd9cc9f2d6f8be3a4cc695
zifarnyirenda <zifarchishalanyirenda@gmail.com>
2025-02-10 16:51:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,10 @@ function contactMe() {
     window.location.href = "mailto:zifarchishalanyirenda@gmail.com";
 }
 
-
 document.getElementById('linkedin-btn').href = "https://www.linkedin.com/in/zifar-nyirenda-086b1a237?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app";
 
-
 document.addEventListener('DOMContentLoaded', () => {
-    const profile = document.querySelector('.header');
-      
+    const profile = document.querySelector('.left-panel');
     profile.style.opacity = 0;
     profile.style.transform = 'translateY(-20px)';
     
